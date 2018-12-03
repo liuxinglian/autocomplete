eb176afc03e508ced72e820953077f2dbb502aaf
Ubuntu <ubuntu@ip-172-31-93-93.ec2.internal>
2018-12-03 18:11:15 +0000

    Add lastest model3

--- model3.py
+++ model3.py
@@ -10,7 +10,76 @@ import gensim.models.keyedvectors as word2vec
 SAVE_PATH = './model/m.cpkt'
 
 
-def get_review_data(filename, start=0, end=10000):
+class DataSet(object):
+    '''
+    This is an object that is holding the data and generate batches.
+    '''
+    def __init__(self, 
+                 data,
+                 label,
+                 seq_length,
+                 training=True):
+        self.data = data
+        self.label = label
+        self.seq_length = seq_length
+        self.training = training
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+        self._num_data = data.shape[0]
+        self._curr_order = np.arange(self._num_data)
+
+    def next_batch(self, batch_size, shuffle=True):
+        start = self._index_in_epoch
+        #print(self.training)
+        if self.training == False:
+            #print("triggered")
+            return self.data, self.label, self.seq_length
+        if start == 0 and self._epochs_completed == 0 and shuffle:
+            np.random.shuffle(self._curr_order)
+        if start + batch_size > self._num_data:
+            self._epochs_completed += 1
+            num_feeded = self._num_data - start
+            num_rest = batch_size - num_feeded
+            X_batch_feeded = self.data[self._curr_order[self._index_in_epoch:]]
+            y_batch_feeded = self.label[self._curr_order[self._index_in_epoch:]]
+            seq_length_feeded = self.seq_length[self._curr_order[self._index_in_epoch:]]
+
+            if shuffle:
+                np.random.shuffle(self._curr_order)
+
+            X_batch_rest = self.data[self._curr_order[:num_rest]]
+            y_batch_rest = self.label[self._curr_order[:num_rest]]
+            seq_length_rest = self.seq_length[self._curr_order[:num_rest]]
+            self._index_in_epoch = num_rest
+            X_batch = np.concatenate((X_batch_feeded, X_batch_rest), axis=0)
+            y_batch = np.concatenate((y_batch_feeded, y_batch_rest), axis=0)
+            seq_length_batch = np.concatenate((seq_length_feeded, seq_length_rest), axis=0)
+        else:
+            X_batch = self.data[self._curr_order[self._index_in_epoch:self._index_in_epoch + batch_size]]
+            y_batch = self.label[self._curr_order[self._index_in_epoch:self._index_in_epoch + batch_size]]
+            seq_length_batch = self.seq_length[self._curr_order[self._index_in_epoch:self._index_in_epoch + batch_size]]
+            self._index_in_epoch += batch_size
+        return X_batch, y_batch, seq_length_batch
+
+def get_rnn_cell(typ='rnn', platform='cpu', **kwargs):
+    # get an rnn cell with the specified type on specific platform
+    if typ == 'rnn' and platform == 'cpu':
+        return tf.nn.rnn_cell.BasicRNNCell(**kwargs)
+    elif typ == 'rnn' and platform == 'gpu':
+        return tf.contrib.cudnn_rnn.CudnnRNNTanhSaveable(**kwargs)
+    elif typ == 'lstm' and platform == 'cpu':
+        return tf.nn.rnn_cell.LSTMCell(**kwargs)
+    elif typ == 'lstm' and platform == 'gpu':
+        return tf.contrib.cudnn_rnn.CudnnLSTMSaveable(**kwargs)
+    elif typ == 'gru' and platform == 'cpu':
+        return tf.nn.rnn_cell.GRUCell(**kwargs)
+    elif typ == 'gru' and platform == 'gpu':
+        return tf.contrib.cudnn_rnn.CudnnGRUSaveable(**kwargs)
+    else:
+        print("Please choose a valid combination of type and platform")
+        raise ValueError()
+
+def get_review_data(filename, start, end):
     with open(filename) as f:
         data = f.readlines()
     reviews = [json.loads(x.strip()) for x in data]
@@ -19,11 +88,10 @@ def get_review_data(filename, start=0, end=10000):
 
     return sentences
 
-
 # return word2Vec model that can extract word embedding
-def get_word_embedding(filename):
+def get_word_embedding(filename, start=0, end=1000):
 
-    sentences = get_review_data(filename)
+    sentences = get_review_data(filename, start, end)
     saved_model = my_file = Path('model.txt')
 
     if not saved_model.is_file():
@@ -45,44 +113,56 @@ def get_word_embedding(filename):
     return model, sentences
 
 
-# start predicting from the 6th word
-def prepare_input_for_nn(model, sentences):
+def prepare_input_for_nn(model, sentences, n_steps=20, reverse=True, training=True):
+    '''
+    Prepare the input for the seq2seq model, with the pre-defined length and order.
+    It is being said that reversed model leads to a better performance.
+    n_steps = number of words we are going to feed into the network for the prediction of next.
+    '''
     # list of numpy array (each is a embedding representing the previous sequence)
     inputs = []
     # list of vector (true word representing by vector)
     true_words = []
+    # list of seq_lengths (the length of each sequence)
+    seq_lengths=[]
     for sentence in sentences:
+        sentence_embedding = []
+        # get rid of reviews with smaller than 5 words
         if len(sentence) < 5:
             continue
-
-        # unnormalized one doesn't divide by the total weight
-        weighted_sum = np.zeros(model.vector_size)
-        total_weight = 0
-        for i in range(5):
-            total_weight += i+1
+        for i in range(len(sentence)):
             if sentence[i] in model.wv.vocab:
-                weighted_sum += model[sentence[i]] * (i+1)
-
+                sentence_embedding.append(model[sentence[i]])
+            else:
+                sentence_embedding.append(np.zeros(model.vector_size, dtype=np.float32))
+        sentence_embedding = np.array(sentence_embedding)
         # begin prepare input and label
         for i in range(5, len(sentence)):
-            cur_input = weighted_sum / total_weight
+            seq_begin = np.max([0, i-n_steps])
+            seq_length = np.min([i, n_steps])
+            cur_input = sentence_embedding[seq_begin:i]
             cur_label = sentence[i]
-
+            if seq_length < n_steps:
+                pad_num = n_steps - seq_length
+                pad = np.zeros((pad_num, model.vector_size))
+                cur_input = np.concatenate((pad, cur_input), axis=0) 
+            if reverse:
+                cur_input=np.flip(cur_input,0)
             if cur_label in model.wv.vocab:
                 inputs.append(cur_input)
                 true_words.append(model[cur_label])
-
-                weighted_sum += model[cur_label] * (i+1)
-
-            total_weight += i+1
-            
-
-    return inputs, true_words
+                seq_lengths.append(seq_length)
+    inputs, true_words, seq_lengths = np.array(inputs, dtype=np.float32), np.array(true_words), np.array(seq_lengths)
+    return DataSet(inputs, true_words, seq_lengths)
 
 
-def build_nn(input_ph, out_size=100):
-    hidden = tf.layers.dense(inputs=input_ph, units=256, activation=tf.nn.relu)
-    output = tf.layers.dense(inputs=hidden, units=out_size)
+def build_nn(training, input_ph, n_steps, n_inputs, n_neurons, seq_length_ph, out_size=100, keep_prob=0.5):
+    cell = get_rnn_cell(typ='rnn', platform='cpu', num_units = n_neurons)
+    
+    outputs, state = tf.nn.dynamic_rnn(cell, input_ph, dtype=tf.float32, sequence_length=seq_length_ph)
+    output = tf.layers.dense(inputs=state, units=out_size)
+    output = tf.cond(training, lambda: tf.nn.dropout(output, keep_prob), lambda:output)
+    output = tf.layers.dense(inputs=output, units=out_size)
     return output
 
 def get_loss(pred_word, true_word):
@@ -95,49 +175,37 @@ def get_optimizer(loss, lr=0.005):
     return tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
 
 
-def train_nn(model, sess, saver, input_ph, word_ph, loss, train_op, inputs, true_words, batch_size, num_epoch):
+
+def train_nn(seq_length_ph,n_steps, n_inputs, training, model, sess, saver, input_ph, word_ph, loss, train_op, dataset, batch_size, num_epoch):
     print("begin training")
-    index = np.arange(len(inputs))
-    # change inputs and true_words vectors to np array
-    ### inputs = np.reshape(np.array(inputs), (len(inputs), model.vector_size))
-    ### true_words = np.reshape(np.array(true_words), (len(inputs), model.vector_size))
 
-    iterations = int(len(inputs)/batch_size)
-
-    for i in range(iterations):
-        batch_index = np.random.choice(index, size=batch_size, replace=True)
-        batch_inputs = itemgetter(*batch_index)(inputs)
-        batch_words = itemgetter(*batch_index)(true_words)
-        batch_inputs_np = np.reshape(np.array(batch_inputs), (batch_size, model.vector_size))
-        batch_words_np = np.reshape(np.array(batch_words), (batch_size, model.vector_size))
-        sess.run(train_op, feed_dict={input_ph: batch_inputs_np, word_ph: batch_words_np})
-        cur_loss = sess.run(loss, feed_dict={input_ph: batch_inputs_np, word_ph: batch_words_np})
-        print("loss for batch {} is {}".format(i, cur_loss))
-
-    for r in range(num_epoch-1):
-        for i in range(iterations):
-            batch_index = np.random.choice(index, size=batch_size, replace=True)
-            batch_inputs = itemgetter(*batch_index)(inputs)
-            batch_words = itemgetter(*batch_index)(true_words)
-            batch_inputs_np = np.reshape(np.array(batch_inputs), (batch_size, model.vector_size))
-            batch_words_np = np.reshape(np.array(batch_words), (batch_size, model.vector_size))
-            sess.run(train_op, feed_dict={input_ph: batch_inputs_np, word_ph: batch_words_np})
-            cur_loss = sess.run(loss, feed_dict={input_ph: batch_inputs_np, word_ph: batch_words_np})
-            print("loss for batch {} is {}".format(i, cur_loss))
-
+    for r in range(num_epoch):
+        for i in range(dataset._num_data // batch_size + 1):
+            X_batch, y_batch, seq_length_batch = dataset.next_batch(batch_size)
+            #?????
+            #print(X_batch.shape)
+            X_batch = X_batch.reshape((-1, n_steps, n_inputs))
+            sess.run(train_op, feed_dict={training: True, input_ph: X_batch, word_ph: y_batch, seq_length_ph: seq_length_batch})
+            cur_loss = sess.run(loss, feed_dict={training: True, input_ph: X_batch, word_ph: y_batch, seq_length_ph: seq_length_batch})
+            if i%1000==0:
+                print("loss for batch {} is {}".format(i, cur_loss))
+        # print(epoch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
     saver.save(sess, SAVE_PATH)
 
 
-def get_prediction(model, nn_model, test_sentences, input_ph, word_ph):
+def get_prediction(seq_length_ph, training, model, nn_model, test_sentences, input_ph, word_ph, n_steps=20,reverse=True):
     print('begin predicting')
-    test_inputs, test_true_words = prepare_input_for_nn(model, test_sentences)
+    dataset = prepare_input_for_nn(model, test_sentences, n_steps, reverse, training = False)
+    X_batch, y_batch, seq_length_batch = dataset.data, dataset.label, dataset.seq_length
+    test_inputs = X_batch
+    test_true_words = y_batch
     print('test true word len = {}'.format(len(test_true_words)))
-    test_inputs = np.reshape(np.array(test_inputs), (len(test_inputs), model.vector_size))
-    test_true_words = np.reshape(np.array(test_true_words), (len(test_true_words), model.vector_size))
+    #test_inputs = np.reshape(np.array(test_inputs), (len(test_inputs), model.vector_size))
+    #test_true_words = np.reshape(np.array(test_true_words), (len(test_true_words), model.vector_size))
     with tf.Session() as sess:
         saver = tf.train.Saver()
         saver.restore(sess, SAVE_PATH)
-        test_pred_words = sess.run(nn_model, feed_dict={input_ph: test_inputs, word_ph: test_true_words})
+        test_pred_words = sess.run(nn_model, feed_dict={training: False, input_ph: test_inputs, word_ph: test_true_words, seq_length_ph: seq_length_batch})
     print('test pred word len = {}'.format(len(test_pred_words)))
     return test_true_words, test_pred_words
 
@@ -162,26 +230,44 @@ def get_accuracy(model, true_words, pred_words, topn=10):
 
 
 def main():
-    model, sentences = get_word_embedding('yelp_academic_dataset_review.json')
-    train_fea, train_label = prepare_input_for_nn(model, sentences)
-    test_sentences = get_review_data('yelp_academic_dataset_review.json', 10000, 12000)
+    filename='yelp_academic_dataset_review.json'
+    model, sentences = get_word_embedding(filename,0, 10000)
+    #TODO: to change
+    n_steps = 20
+    reverse = True
+    dataset = prepare_input_for_nn(model, sentences, n_steps, reverse)
+    test_sentences = get_review_data(filename, 10000, 12000)
+
     print("----------------------- DONE WITH GET REVIEW DATA -----------------------")
-    input_ph = tf.placeholder(tf.float32, [None, model.vector_size], name='train_input')
+    
+    
+    #embedded vector size
+    n_inputs = model.vector_size
+    #each vector is converted into dim=n_neurons
+    n_neurons = 128
+    # do reset_graph()?
+    
+    input_ph = tf.placeholder(tf.float32, [None, n_steps, model.vector_size], name='train_input')
     word_ph = tf.placeholder(tf.float32, [None, model.vector_size], name='train_label')
     training = tf.placeholder(tf.bool)
-    nn_model = build_nn(input_ph)
+    seq_length_ph = tf.placeholder(tf.int32, [None])
+    
+    nn_model = build_nn(training, input_ph, n_steps, n_inputs, n_neurons, seq_length_ph)
+    #state is the state of last time stamp (word) for EACH sentence
+
     loss = get_loss(nn_model, word_ph)
     train_op = get_optimizer(loss)
     saver = tf.train.Saver()
     # begin training
     init = tf.global_variables_initializer()
+    
     with tf.Session() as sess:
         sess.run(init)
-        train_nn(model, sess, saver, input_ph, word_ph, loss, train_op, train_fea, train_label, 32, num_epoch=2)
+        train_nn(seq_length_ph,n_steps, n_inputs, training, model, sess, saver, input_ph, word_ph, loss, train_op, dataset, 6, num_epoch=2)
     print("----------------------- DONE WITH TRAINING -----------------------")
     # t_input_ph = tf.placeholder(tf.float32, [None, model.vector_size], name='test_input')
     # t_word_ph = tf.placeholder(tf.float32, [None, model.vector_size], name='test_predicted_label')
-    test_true_words, test_pred_words = get_prediction(model, nn_model, test_sentences, input_ph, word_ph)
+    test_true_words, test_pred_words = get_prediction(seq_length_ph, training, model, nn_model, test_sentences, input_ph, word_ph)
     print("----------------------- DONE WITH PREDICTION -----------------------")
     acc = get_accuracy(model, test_true_words, test_pred_words)
     print("----------------------- DONE WITH GET ACCURACY -----------------------")
@@ -189,3 +275,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
