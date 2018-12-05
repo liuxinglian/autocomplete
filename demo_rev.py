9f6170ba3d7d788d5d8a5470b144370e8362a643
haiyangh <32042591+haiyangh@users.noreply.github.com>
2018-12-04 19:55:39 -0500

    Revised Demo for model2

--- demo_rev.py
+++ demo_rev.py
@@ -41,28 +41,32 @@ with tf.Session() as sess:
         # begin prepare input
         n_steps = 20
         reverse = True
+        pad_num = 0
+        denom = 0
         if len(sentence) < n_steps + 1:
             cur_input = sentence_embedding[:-1]
             pad_num = n_steps - len(sentence) + 1
             pad = np.zeros((pad_num, word2vec_model.vector_size))
+            denom = np.sum(np.arange(21)) - np.sum(np.arange(pad_num + 1))
             if not reverse:
                 cur_input = np.concatenate((pad, cur_input), axis=0)
             else:
                 cur_input = np.concatenate((cur_input, pad), axis=0)
         else:
-            cur_input = sentence_embedding[len(sentence)-1-n_steps:-1]
+            cur_input = sentence_embedding[len(sentence)-n_steps:-1]
+            denom = 210
         if reverse:
             cur_input = np.flip(cur_input,0)
         # model 2 only
         coeff = np.arange(20) + 1
         coeff = coeff.reshape(20,1)
-        cur_input = np.sum(coeff*cur_input, axis=0).reshape(1,100)
-        cur_input = cur_input/210
+        cur_input = np.sum(coeff*cur_input, axis=0).reshape(1, 100)
+        cur_input = np/denom
 
             
         nn_model = tf.get_default_graph().get_tensor_by_name("dense_3/BiasAdd:0")
         input_ph = tf.get_default_graph().get_tensor_by_name("train_input:0")
            
-        pred = sess.run(nn_model, feed_dict={input_ph: cur_input})        
-        pred_word = pred_dict_filter(word2vec_model, sentence[-1], pred[-1], topn=1, cons=200)
+        pred = sess.run(nn_model, feed_dict={input_ph: cur_input}) 
+        pred_word = pred_dict_filter(word2vec_model, sentence[-1], pred, topn=1, cons=200)
         print(pred_word)
