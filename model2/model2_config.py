4cf333110f1a12f50346a0d277a485970845e544
Mark Mao <myz@umich.edu>
2018-12-10 17:43:35 -0500

    some refactor

--- model2/model2_config.py
+++ model2/model2_config.py
@@ -5,6 +5,16 @@ parameters specific to model 2
 '''
 
 model2_params = {
-    TF_SAVE_PATH : './model/m.cpkt',
-    ADD_STAR : False
+    train_size = 10000,
+    test_size = (int)(0.2 * train_size),
+    
+    train_start = 0,
+    train_end = train_start + train_size,
+
+    test_start = train_end,
+    test_end = test_start + test_size,
+
+    tf_save_path : './model' + '_train_' + train_size + '_test_' + test_size + '/m.cpkt',
+    
+    add_star : False
 }
