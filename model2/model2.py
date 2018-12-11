19468c03b9020fa9fee5b26d01b4031f7fe10ea6
Yuzhou Mao <myz@umich.edu>
2018-12-11 14:07:25 -0500

    Update model2.py

--- model2/model2.py
+++ model2/model2.py
@@ -6,10 +6,10 @@ import nltk
 import tensorflow as tf
 from gensim.models import Word2Vec
 import gensim.models.keyedvectors as word2vec
-sys.path.append("..")
-from autocomplete.system_config import system_params
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from model2_config import model2_params
-from autocomplete.prep_data import get_review_data, get_word_embedding
+from system_config import system_params
+from prep_data import get_review_data, get_word_embedding
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
 
